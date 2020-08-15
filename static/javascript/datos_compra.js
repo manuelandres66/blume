9482cc713b5b73ceb50335d9bcf44526d7e28355
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2020-08-15 15:47:30 -0500

    Changes javascript

--- static/javascript/datos_compra.js
+++ static/javascript/datos_compra.js
@@ -4,6 +4,7 @@ var precio = document.getElementById('total-precio');
 const productos = document.getElementsByClassName('producto_precio');
 var cantidad = document.getElementsByClassName('producto_cantidad');
 const stock_disponible = document.getElementsByClassName('stock_cantidad');
+var stocks = document.getElementsByClassName('stock');
 
 items.textContent = "Subtotal (" + productos.length + " items)";
 
@@ -18,15 +19,13 @@ for (var i = 0; i < productos.length; i++) {
     var stock_dispon = parseInt(stock_disponible[i].textContent); //Check si es es posible
 
     if (cant > stock_dispon) {
+        stocks[i].textContent = "No hay stock suficiente."
         cantidad[i].style.color = "#F00" //Si no lo es lo pone en rojo
     }
     precio_total += numero * cant
 };
     
 precio.textContent = ("$" + precio_total).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-
-var stocks = document.getElementsByClassName('stock');
 
 Array.from(stocks).forEach(element => {
     if (element.textContent == "Stock Disponible") {
