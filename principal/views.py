8cf13991c57f477d9a6524aff4acc0c1189c8220
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2020-08-26 11:29:45 -0500

    A

--- principal/views.py
+++ principal/views.py
@@ -221,6 +221,7 @@ def tarjeta(request):
 
         resp = hacer_post(envio.valor_total, request.POST['token'], request.POST['payment_method_id'], request.POST["email"])
 
+        return HttpResponse(resp["status"])
         if resp["status"] == "approved":
             for producto in productos:
                 producto.producto.stock -= producto.cantidad
