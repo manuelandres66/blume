f1e8d5270493175918264d18b78e27f824385d75
manuelandres66 <57593279+manuelandres66@users.noreply.github.com>
2020-09-10 15:02:55 -0500

    Urls

--- blume/urls.py
+++ blume/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('principal.urls')),
+    path('envios/', include('envios.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
